558ffb106e05e381cc680863252cd26ebd88e2fb
ZJ Yan <yzj.cpp@gmail.com>
2025-07-21 21:55:16 -0700

    update alien dict

--- leetcode/problems/lc269.py
+++ leetcode/problems/lc269.py
@@ -1,25 +1,21 @@
-from collections import defaultdict, deque
+from collections import defaultdict, deque, Counter
 
 
 def alienOrder(words):
     # Build graph
     adj = defaultdict(set)
-    indegree = {}
-    for word in words:
-        for c in word:
-            indegree[c] = 0
+    indegree = Counter({c: 0 for word in words for c in word})
 
-    for i in range(len(words) - 1):
-        w1, w2 = words[i], words[i + 1]
-        min_len = min(len(w1), len(w2))
-        if w1[:min_len] == w2[:min_len] and len(w1) > len(w2):
-            return ""
-        for j in range(min_len):
-            if w1[j] != w2[j]:
-                if w2[j] not in adj[w1[j]]:
-                    adj[w1[j]].add(w2[j])
-                    indegree[w2[j]] += 1
+    for w1, w2 in zip(words, words[1:]):
+        for c1, c2 in zip(w1, w2):
+            if c1 != c2:
+                if c2 not in adj[c1]:
+                    adj[c1].add(c2)
+                    indegree[c2] += 1
                 break
+        else:  # one word is the prefix of the other
+            if len(w2) < len(w1):
+                return ""
 
     # Topological sort
     queue = deque([c for c in indegree if indegree[c] == 0])
